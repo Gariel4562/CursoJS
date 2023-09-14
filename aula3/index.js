af88b8f0b5de1664fadaede93cdb7f191b80c529
Gabriel <gabriel.mgran20@hotmail.com>
2023-09-14 16:53:31 -0300

    Update
    Console.log + exercicio 1

---
+++ aula3/index.js
@@ -0,0 +1,13 @@
+console.log('Gabriel "Soares" '); // String = Texto
+console.log("Gabriel 'Soares' "); // String = Texto
+console.log(`'Gabriel' "Soares" `); // String = Texto
+
+console.log(35, 15.85, 'Gabriel Soares'); // Number e String
+
+
+
+
+
+
+
+
---
+++ aula3/indexEx.js
@@ -0,0 +1,7 @@
+// exercicio 1
+
+console.log('Meu Nome é "Gabriel". estou aprendendo JavaScript ás', 16, 'da tarde.');
+console.log('este', 'é', 'um', 1000);
+
+
+
