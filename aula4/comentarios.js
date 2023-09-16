b96b1bb809e1cb84b322668ef5007da8c0418a5b
Gabriel <gabriel.mgran20@hotmail.com>
2023-09-16 18:09:55 -0300

    Update
    Comentários + Ligação HTML + Js

---
+++ aula4/comentarios.js
@@ -0,0 +1,19 @@
+// comenta uma linha
+
+/* Comenta uma determinada area. laosloasla
+aosasolaosloasalslalos
+alosalsoalloslaosloalosloa
+
+*/
+
+console.log("Olá mundo");
+
+/* posso também ignorar todo um código
+
+let valor = 10;
+
+s = valor + 25;
+
+console.log(`${s}`);
+
+*/ 
---
+++ aula5/js/index.js
@@ -0,0 +1,10 @@
+// obs: na pagina do google se pressionar as teclas ctrl + u, abrirá código da pagina.
+
+console.log(`oi`);
+console.log(`Este trecho será exibido no console do navegador`);
+
+window.alert(`OIE`);
+
+
+
+
