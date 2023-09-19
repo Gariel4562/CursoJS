e4a0b9ab6062204ad48f8f9945f09827e594e818
Gabriel <gabriel.mgran20@hotmail.com>
2023-09-19 15:50:14 -0300

    Update
    Let, Const, type variável.

---
+++ aula6/index.js
@@ -0,0 +1,44 @@
+// Variável algo que pode ser modificado ao decorrer do código. 
+
+let nome1 = 'João';
+let nome2 = 'Maria';
+let nome3 = 'Eduardo';
+
+
+console.log(`${nome1} nasceu em 1984.`);
+console.log(`Em 2000 ${nome1} conheceu ${nome2}. `);
+console.log(`Casou-se com ${nome2} em 2012.`);
+console.log(`${nome2} teve um filho com ${nome1} em 2015.`);
+console.log(`O filho de ${nome1} se chama ${nome3}. `);
+
+// declarei a variável antes, e coloquei o valor depois. e alterei o valor dela no decorrer do programa.
+
+let nome;
+nome = 'Ogro';
+nome = 'Orc';
+console.log(nome);
+
+// Não podemos criar variáveis com palavras reservadas ex.. 'let if'
+// Variáveis precisam ter nomes significados ex.. 'Nome' para nome de pessoas.
+// Não podemos começar o nome de uma Variável com numero ex.. 12Number.
+
+/*errado, dará erro v
+ let 12Number = 12; */
+
+// Variáveis não podem ter espaços ou traços, em vez disso usamos camelCase ex..
+
+let nomeCompleto = "João da Silva Ribeiro";
+console.log(`${nomeCompleto}`);
+
+// case-sensitive.
+// Não podemos redeclarar variáveis com let.
+// Não utilize var, Utilize let.
+
+
+
+
+
+
+
+
+
---
+++ aula7/index.js
@@ -0,0 +1,49 @@
+// Não podemos modificar o valor de uma Constante. ex.. dará erro.
+
+/*
+
+const nomeCompleto = 'João da Silva';
+console.log(`${nomeCompleto}`);
+nomeCompleto = 'Silva Ribeiro';
+console.log(`${nomeCompleto}`);
+
+*/
+
+// Conta básica com Const, porém não posso alterar esses valores mais pra frente. o valor que defini inicialmente continuará até o fim.
+
+const primeiroNumero = 9;
+const segundoNumero = 10;
+
+resultado = primeiroNumero + segundoNumero;
+
+const resultadoDuplicado = resultado * 2
+
+// se declarar a variável com Let eu posso mudar o valor dela.
+
+let resultadoTriplicado = resultadoDuplicado * 3;
+resultadoTriplicado = resultadoTriplicado + 15;
+
+
+console.log(`${resultado}`);
+console.log(`${resultadoDuplicado}`);
+console.log(`${resultadoTriplicado}`);
+
+console.log(typeof(primeiroNumero + segundoNumero)); // descobrir o tipo da variável.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
