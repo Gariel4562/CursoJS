3b7190a25a75c267e2dcb70bc8a4af7cefde8b17
Gabriel <gabriel.mgran20@hotmail.com>
2023-10-05 15:48:35 -0300

    update
    Operadores aritméticos

---
+++ aula11/index.js
@@ -0,0 +1,54 @@
+/* 
+
+operadores aritméticos 
++ (adição e concatenação) 
+- (Subtração)
+/ (Divisão) 
+* (multiplicação)
+** (potenciação)
+% (modulo, resto da divisão)
+
+
+*/
+
+/* 
+
+1 ()
+2 **
+3 * / %
+4 + -
+
+
+*/
+
+const number1 = 10;
+const number2 = 5;
+
+console.log(number1 % number2);
+
+/* Incremento e Decremento */
+
+let contador = 1;
+console.log(++contador);
+console.log(--contador);
+
+// incremento de 50 + 50
+
+let contador2 = 2;
+contador2 *= 2; 
+contador2 *= 2;
+contador2 *= 2;
+console.log(contador2);
+
+// conversão com Number em vez de parseInt e parseFloat
+
+const num1 = 10;
+const num2 = Number('5.9');
+console.log(num1 + num2);
+
+
+
+
+
+
+
