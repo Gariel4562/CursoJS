ed2ce2b687237860fd196595f2780f5f675c620c
Gabriel <gabriel.mgran20@hotmail.com>
2023-10-07 14:15:48 -0300

    Update
    Janelas de Confimação, mensagens e janela de pergunta.
    Exercicio mini calculadora.

---
+++ aula12/index.js
@@ -0,0 +1,24 @@
+/* 
+alert(`Olá aluno`); // Cria uma mensagem na tela
+
+window.confirm(`deseja continuar? `); // cria uma mensagem e pergunta se você aceita ou cancela dependendo da resposta será True ou False.
+
+window.prompt(`Digite seu nome: `) // faz uma pergunta e mostra um campo para você responder.
+*/
+
+// exercicio 
+
+let n1 = Number(window.prompt(`Digite um numero: `));
+let n2 = Number(window.prompt(`Digite outro numero: `));
+
+let res = n1 + n2;
+
+window.alert(`O resultado de ${n1} + ${n2} é igual a ${res}.`);
+
+
+
+
+
+
+
+
