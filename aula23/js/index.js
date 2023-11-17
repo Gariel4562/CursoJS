41ee274f4922584add05049d810728ed712004fc
Gabriel <gabriel.mgran20@hotmail.com>
2023-11-17 01:11:33 -0300

    Update
    Operadores de comparação

---
+++ aula23/js/index.js
@@ -0,0 +1,21 @@
+/* Operadores de Comparação 
+== igual
+>= maior ou igual
+<= menor ou igual
+< menor que
+> maior que
+=== igualdade estrita
+!= diferente
+!== diferente estrito
+
+
+*/
+
+console.log(5 > 3);
+console.log(4 < 5);
+console.log(5 == 5);
+console.log(6 >= 5);
+console.log(4 <= 9);
+console.log(8 === '8'); // Vai dar falso pois, o tipo não é igual, apenas o número é igual.
+console.log(8 != 9);
+console.log(10 !== `10`); // Vai dar verdadeiro por causa do tipo.
