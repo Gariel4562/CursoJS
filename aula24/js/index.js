68f19e55f0bdf086c3f35cde4a58174623683884
Gabriel <gabriel.mgran20@hotmail.com>
2023-11-18 15:16:40 -0300

    Update
    Operadores Lógicos

---
+++ aula24/js/index.js
@@ -0,0 +1,15 @@
+/* 
+   Operadores Lógicos
+    && -> e (Todas as expressões precisam ser verdadeiras para retornar verdadeiro.)
+    || -> ou (Se cumprir pelo menos uma das condições dará verdadeiro.)
+    !  -> not (negação, ele nega ex.. !chove)
+
+*/
+
+const expressaoAnd = true && true && true && true;
+const expressaoOr = true || false;
+const expressaoNot = !true;
+
+console.log(expressaoOr);
+
+
