dd10047dea67ced4afe7e9a22d7775602945b9ca
Gabriel <gabriel.mgran20@hotmail.com>
2023-12-12 16:03:02 -0300

    Update
    Operação Ternária

---
+++ aula29/index.js
@@ -0,0 +1,38 @@
+// Operador ternário para encurtar código. simplifica if/else ? (valor para verdeiro) : (valor para falso)
+
+const pontuacaoUsuario = 999;
+
+// Modo de fazer com Operação Ternária
+
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario Vip' : 'Usuario não vip';
+
+console.log(nivelUsuario);
+
+
+
+
+
+
+
+
+
+
+
+// Modo Normal de fazer com if e else.
+
+if(pontuacaoUsuario >= 1000) {
+    console.log(`Meu usuário, é um Usuário Vip. `);
+
+} else {
+    console.log(`Meu usuário, não é um usuário vip.`);
+
+}
+
+
+
+
+
+
+
+
+
