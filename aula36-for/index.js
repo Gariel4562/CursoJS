76edcb92ca89b10abb9e1fb2ec21f13cff6f96c4
Gabriel <gabriel.mgran20@hotmail.com>
2024-01-18 17:36:18 -0300

    Update, Laço For.
    Laço de repetição For.

---
+++ aula36-for/index.js
@@ -0,0 +1,26 @@
+/* 
+
+// for pulando de 1 em 1
+for(let linha = 1; linha <= 5; linha++) {
+    console.log(`linha ${linha} `);
+
+}
+
+// for pulando de 2 em 2.
+
+for(let linha = 6; linha <= 10; linha += 2) {
+    console.log(`linha ${linha} `);
+
+}
+
+*/
+
+const frutas = ['Maça', 'Pera', 'Uva'];
+
+for(let i = 0; i < frutas.length; i++) {
+    console.log(`indice ${i}`, frutas[i]);
+
+}
+
+
+
