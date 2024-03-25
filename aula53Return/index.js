4102db02557b6979ff2214df52b458f636134ffe
Gabriel <gabriel.mgran20@hotmail.com>
2024-03-25 19:44:27 -0300

    Update, return
    return

---
+++ aula53Return/index.js
@@ -0,0 +1,29 @@
+// return retorna um valor e termina a função.
+
+function soma(a, b) {
+    return a + b; // assim que retornar já termina a função, estou pedindo para a função me retornar a + b e ela fez justamente isso.
+
+}
+
+console.log(soma(5, 2));
+
+
+// multiplicador e return
+
+function criaMultiplicador(multiplicador) {
+
+    return function (n) {
+        return n * multiplicador;
+
+    }
+
+}
+
+const duplica = criaMultiplicador(3);
+const triplica = criaMultiplicador(2);
+const quadriplica = criaMultiplicador(10);
+
+console.log(duplica(3));
+console.log(triplica(2));
+console.log(quadriplica(10));
+
