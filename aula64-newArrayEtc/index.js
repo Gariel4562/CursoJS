85a8330b4774944d69afc78bad8147a8f55d468d
Gabriel Soares <88640613+Gariel4562@users.noreply.github.com>
2024-05-20 15:59:17 -0300

    Update, new Array
    new Array

---
+++ aula64-newArrayEtc/index.js
@@ -0,0 +1,5 @@
+const nomes = new Array('Eduardo', 'Maria', 'Joana', 'Pedro');
+const novo = nomes.slice(0, -2); // nesse caso ele vai começar no Eduardo e vai remover dois elementos do Array, e só teremos Eduardo e Maria.
+
+
+console.log(novo);
