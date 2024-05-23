14d680f95386d1ca60d53a0b7bd566ffd9f3e4b5
Gabriel Soares <88640613+Gariel4562@users.noreply.github.com>
2024-05-22 22:35:12 -0300

    update, Concatenação Arrays
    Concatenando Arrays

---
+++ aula66-concatenando-arrays/index.js
@@ -0,0 +1,10 @@
+const nome = ['Gabriel', 'Pedro', 'Vivian', 'Josefa' ];
+
+const nome2 = ['Cunha', 'Benedito', 'Simba'];
+
+const juntar = nome.concat(nome2, ['Carla', 'Kate', 'João']); // estou concatenando o array 1 com o 2, e adicionando mais 3 nomes após a Junção dos arrays.
+
+console.log(juntar);
+
+
+
