4de2bfaca796f4ab7163e5a747432a231679bf8b
Gabriel Soares <88640613+Gariel4562@users.noreply.github.com>
2024-06-16 00:51:57 -0300

    update, forEach
    forEach

---
+++ aula71-forEACH/index.js
@@ -0,0 +1,7 @@
+// for Each só está disponivel dentro de arrays.
+
+const a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+a1.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array);
+});
