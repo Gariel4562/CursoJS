171f1e0c09476de96bf482590e97c624ff73616c
Gabriel Soares <88640613+Gariel4562@users.noreply.github.com>
2024-10-08 22:35:05 -0300

    Update, Webpack
    Webpack

---
+++ aula95WebPack/main.js
@@ -0,0 +1,3 @@
+// npm init -y inicia o npm.
+// instalação das pendências: " npm i --save-dev @babel/preset-env @babel/core @babel/cli babel-loader webpack webpack-cli regenerator-runtime core-js@2 "
+// "webpack.config.js" cria o arquivo webpack.
