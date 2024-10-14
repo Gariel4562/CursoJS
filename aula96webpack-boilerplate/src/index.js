2cbda8fe84d6febcf1f7f2ad1e9012d489b6b0a2
Gabriel Soares <88640613+Gariel4562@users.noreply.github.com>
2024-10-14 18:23:45 -0300

    Update, Import/export
     Import/export

---
+++ aula96webpack-boilerplate/src/index.js
@@ -0,0 +1,7 @@
+import { nome, sobreNome, idade, soma } from './modulo1';
+
+console.log(nome, sobreNome, idade);
+console.log(soma(5, 5));
+
+
+
---
+++ aula96webpack-boilerplate/src/modulo1.js
@@ -0,0 +1,11 @@
+const nome = 'Gabriel'
+const sobreNome = 'Lima'
+const idade = 29
+
+function soma(x, y) {
+
+    return x + y;
+
+}
+
+export { nome, sobreNome, idade, soma };
